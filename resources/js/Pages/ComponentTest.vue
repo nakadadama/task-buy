0d08adbf44f89555d05da8b33f64777241baaf7e
nakada <sabonsaboten@gmail.com>
2023-11-23 17:56:51 +0900

    First Commit

---
+++ resources/js/Pages/ComponentTest.vue
@@ -0,0 +1,15 @@
+<script setup>
+import Label from '@/components/Label.vue'
+import Input from '@/components/Input.vue'
+import GuestLayout from '@/Layouts/Guest.vue'
+
+const emitTest = e => console.log(e)
+</script>
+<template>
+ <GuestLayout>
+ <Label value="件名">タイトル</Label>
+ <Input modelValue="初期値です"
+ @update:modelValue="emitTest"
+ ></Input>
+ </GuestLayout>
+</template>
---
+++ resources/js/Pages/InertiaTest.vue
@@ -0,0 +1,24 @@
+<script setup> 
+import { Link } from '@inertiajs/vue3';
+import {ref}  from 'vue'
+
+const newTitle = ref('')
+const newContent = ref('') 
+ 
+
+</script>
+
+<template>
+Inertiaテストです。<br>
+ <a href="/">aタグでWelcomeに移動</a><br>
+ <Link href="/">LinkでWelcomeに移動</Link><br>
+ <Link :href="route('inertia.show', { id : 50 })">button</Link><br> 
+
+
+
+ <input type="text" name="newTitle" v-model="newTitle">{{ newTitle }}<br>
+ <input type="text" name="newContent" v-model="newContent">{{ newContent }}
+<br> 
+<Link as="button" method="post" :href="route('inertia.store')" :data="{ title:
+newTitle, content: newContent}">保存テスト</Link>
+</template>
